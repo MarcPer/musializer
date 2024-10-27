7812bf8a4c1492921fce4342691db4b6807473c9
Marcelo Pereira <marcelovitor.pereira@gmail.com>
2024-10-27 14:44:19 +0100

    Display music samples

--- README.md
+++ README.md
@@ -20,3 +20,11 @@ zig build run
 ```
 
 Note: The raylib-zig library is patched to work with zig 0.13. See https://github.com/ryupold/raylib.zig/pull/46.
+
+
+## References
+
+- [Tsoding's musializer](https://github.com/tsoding/musializer)
+- [Tsoding Musializer video](https://www.youtube.com/watch?v=Xdbk1Pr5WXU)
+- [raylib.zig](https://github.com/ryupold/raylib.zig)
+- [Audio Stream Processing](https://github.com/raysan5/raylib/blob/master/examples/audio/audio_stream_effects.c)
--- src/main.zig
+++ src/main.zig
@@ -13,7 +13,9 @@ pub fn main() !void {
 
     defer raylib.CloseWindow();
 
-    var enableEffectDelay = false;
+    var isPaused = false;
+
+    raylib.AttachAudioStreamProcessor(music.stream, &storeBuffer);
 
     while (!raylib.WindowShouldClose()) {
         raylib.BeginDrawing();
@@ -30,62 +32,68 @@ pub fn main() !void {
         raylib.DrawRectangle(200, 200, @as(u16, @intFromFloat(timePlayed * 400.0)), 12, raylib.MAROON);
         raylib.DrawRectangleLines(200, 200, 400, 12, raylib.GRAY);
 
-        // Add/Remove effect: delay
-        if (raylib.IsKeyPressed(raylib.KeyboardKey.KEY_D)) {
-            enableEffectDelay = !enableEffectDelay;
-            if (enableEffectDelay) {
-                std.debug.print("Attaching audio stream processor\n", .{});
-                raylib.AttachAudioStreamProcessor(music.stream, &audioProcessEffectDelay);
+        drawBuffer();
+
+        // Pause/Resume music
+        if (raylib.IsKeyPressed(raylib.KeyboardKey.KEY_P)) {
+            isPaused = !isPaused;
+            if (isPaused) {
+                raylib.PauseMusicStream(music);
             } else {
-                std.debug.print("Detaching audio stream processor\n", .{});
-                raylib.DetachAudioStreamProcessor(music.stream, &audioProcessEffectDelay);
+                raylib.ResumeMusicStream(music);
             }
         }
     }
 }
 
-const delayBufferSize = 48000 * 2 * 5;
-var delayBuffer: [delayBufferSize]f32 = undefined;
-var delayReadIndex: u32 = 2;
-var delayWriteIndex: u32 = 0;
+const globalBufferSize = 1024;
+var globalBuffer: [globalBufferSize]f32 = undefined;
+var globalBufferIndex: u32 = 0;
 
-fn audioProcessEffectDelay(rawBuffer: ?*anyopaque, frames: u32) void {
+fn storeBuffer(rawBuffer: ?*anyopaque, frames: u32) void {
     if (rawBuffer == null) {
-        std.debug.print("Error: Null buffer in audioProcessEffectDelay\n", .{});
+        std.debug.print("Error: Null buffer in storeBuffer\n", .{});
         return;
     }
 
-    // Cast the raw pointer to the correct type
+    std.debug.assert(frames * 2 <= globalBufferSize);
+
     const bufferData: [*]f32 = @ptrCast(@alignCast(rawBuffer.?));
-    std.debug.print("Processing audio effect delay. Frames: {}\n", .{frames});
+
+    const spareCapacity = globalBufferSize - globalBufferIndex;
 
     var i: usize = 0;
-    while (i < frames * 2) : (i += 2) {
-        if (delayReadIndex == delayBufferSize) {
-            delayReadIndex = 0;
+    if (frames * 2 <= spareCapacity) {
+        while (i < frames * 2) : (i += 2) {
+            globalBuffer[globalBufferIndex] = bufferData[i];
+            globalBufferIndex += 1;
         }
-
-        if (delayReadIndex >= delayBufferSize or delayWriteIndex >= delayBufferSize) {
-            std.debug.print("Error: Index out of bounds. Read: {}, Write: {}, BufferSize: {}\n", .{ delayReadIndex, delayWriteIndex, delayBufferSize });
-            return;
+    } else {
+        while (i < spareCapacity) : (i += 2) {
+            globalBuffer[globalBufferIndex] = bufferData[i];
+            globalBufferIndex += 1;
         }
-        const leftDelay = delayBuffer[delayReadIndex];
-        delayReadIndex += 1;
-        const rightDelay = delayBuffer[delayReadIndex];
-        delayReadIndex += 1;
-
-        // Modify the buffer in place
-        bufferData[i] = 0.5 * bufferData[i] + 0.5 * leftDelay;
-        bufferData[i + 1] = 0.5 * bufferData[i + 1] + 0.5 * rightDelay;
-
-        delayBuffer[delayWriteIndex] = bufferData[i];
-        delayWriteIndex += 1;
-        delayBuffer[delayWriteIndex] = bufferData[i + 1];
-        delayWriteIndex += 1;
-        if (delayWriteIndex == delayBufferSize) {
-            delayWriteIndex = 0;
+        globalBufferIndex = 0;
+        while (i < (frames * 2 - spareCapacity)) : (i += 2) {
+            globalBuffer[globalBufferIndex] = bufferData[i];
+            globalBufferIndex += 1;
         }
     }
+}
 
-    std.debug.print("Finished processing audio effect delay\n", .{});
+fn drawBuffer() void {
+    const cellWidth: f32 = @as(f32, @floatFromInt(raylib.GetScreenWidth())) / @as(f32, @floatFromInt(globalBufferSize));
+    var cellIntWidth: i32 = @as(i32, @intFromFloat(cellWidth));
+    if (cellIntWidth == 0) {
+        cellIntWidth = 1;
+    }
+    const cellMaxHeight: u32 = @intCast(@divTrunc(raylib.GetScreenHeight(), 2));
+
+    var i: usize = 0;
+    while (i < globalBufferSize) : (i += 1) {
+        const cellHeight: f32 = globalBuffer[i] * @as(f32, @floatFromInt(cellMaxHeight));
+        const cellIntHeight: i32 = @as(i32, @intFromFloat(cellHeight));
+        const positionX = @as(u16, @intFromFloat(@as(f32, @floatFromInt(i)) * cellWidth));
+        raylib.DrawRectangle(positionX, 500 - cellIntHeight, cellIntWidth, cellIntHeight, raylib.RED);
+    }
 }
